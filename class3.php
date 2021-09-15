1bddc77aa8161047cdb2f3fc31746df533fdc8de
lalita saini <virasatlalita@gmail.com>
2021-09-15 17:11:51 +0530

    Revert "Revert "Revert " changes"""
    This reverts commit 434a5f8e2c140815d9f775f34f160ab642135bb7.

--- class3.php
+++ class3.php
@@ -1,9 +1,1 @@
-Grammarly now organizes your writing feedback by theme, 
-so you can see how each change will help your readers better understand your message.
-A paragraph may be well described as a group of several well organized,
- complete, correct, and concise sentences combined together focusing on one subject,
- theme, or central idea. 
- It can be an independent short composition written on certain central idea or it may also be a part of lengthy composition 
- like essay, story or any kind of prose/poetry work. 
- In lengthy composition paragraphs are made to make the  long write-ups easily comprehendible.
- As far as length of a paragraph is concerned there is no set rule for it. They may be from one to many lines long.
+Hello everyone
